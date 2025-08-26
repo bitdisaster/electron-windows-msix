e4fd88927c29947af8d680db7fea1d466b4af2c8
Jan Hannemann <jhannemann@slack-corp.com>
2025-08-26 13:57:38 -0700

    fix: switching to @malept/cross-spawn-promise

--- src/powershell.ts
+++ src/powershell.ts
@@ -1,5 +1,5 @@
 import { log } from './logger';
-import spawn from 'cross-spawn-promise';
+import { spawn } from '@malept/cross-spawn-promise';
 
 export async function powershell(scriptOrCommand: string) {
   log.debug('Running powershell command', { commandAndArgs: scriptOrCommand });
@@ -10,7 +10,7 @@ export async function powershell(scriptOrCommand: string) {
   } else {
     args.push('-Command', scriptOrCommand);
   }
-  const result = await spawn('pwsh.exe', args, { encoding: 'utf-8' });
+  const result = await spawn('pwsh.exe', args);
   log.debug('Powershell command result', { result });
   return result ? result.toString() : '';
 }
--- test/unit/powershell.spec.ts
+++ test/unit/powershell.spec.ts
@@ -1,21 +1,21 @@
 import { describe, expect, it, vi } from "vitest";
 import { powershell } from "../../src/powershell";
-import spawn from "cross-spawn-promise";
+import { spawn } from "@malept/cross-spawn-promise";
 
-vi.mock(import("cross-spawn-promise"), async () => {
+vi.mock(import("@malept/cross-spawn-promise"), async () => {
   return {
-    default: vi.fn(),
+    spawn: vi.fn(),
   }
 })
 
 describe('powershell', () => {
   it('should call powershell', async () => {
     await powershell('C:\\out\\create_dev_cert.ps1');
-    expect(spawn).toHaveBeenCalledWith('pwsh.exe', ['-NoProfile', '-ExecutionPolicy', 'Bypass', 'C:\\out\\create_dev_cert.ps1'], { encoding: 'utf-8' });
+    expect(spawn).toHaveBeenCalledWith('pwsh.exe', ['-NoProfile', '-ExecutionPolicy', 'Bypass', 'C:\\out\\create_dev_cert.ps1']);
   });
 
   it('should call powershell', async () => {
     await powershell('Get-Process');
-    expect(spawn).toHaveBeenCalledWith('pwsh.exe', ['-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Get-Process'], { encoding: 'utf-8' });
+    expect(spawn).toHaveBeenCalledWith('pwsh.exe', ['-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'Get-Process']);
   });
 });
